150aed7bc1152d826770407ff3b29187e993b3c4
Sharasul <sh.haqnazarov@bipartners.uz>
2024-04-20 19:48:02 +0500

    new folder code

---
+++ code/main.py
@@ -0,0 +1,20 @@
+n=55
+m = 'sdsdf'
+d='fd\'df\''
+
+
+
+print(n)
+print(type(m))
+print(d)
+
+a=5
+b=5.89
+c='hello'
+
+print(f"{a} - {b} - {c}")
+print("{}  --  {} - {}" .format(a,b,c))
+
+print('vvedite pervuyu strokured')
+e= input()
+print(e)
