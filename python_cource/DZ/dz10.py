0e4d624fcb5857281d49171d7963869cfa38b78d
shobrat <73270206+shobrat@users.noreply.github.com>
2024-06-29 15:04:24 +0500

    Update dz10.py
    add text

--- python_cource/DZ/dz10.py
+++ python_cource/DZ/dz10.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+data['is_robot'] = (data['whoAmI'] == 'robot').astype(int)
+data['is_human'] = (data['whoAmI'] == 'human').astype(int)
+
+data = data.drop('whoAmI', axis=1)
+
+print(data.head(10))
 
